b67a102288878b10598f0f655d1488f8b7b0ce26
darchont <archontoulidanae@gmail.com>
2024-02-07 14:20:21 +0200

    Initial Commit for this project

---
+++ src/Exercise.java
@@ -0,0 +1,11 @@
+public class Exercise {
+    public static void main(String[] args) {
+        System.out.println(printResult());
+    }
+
+    public static String printResult() {
+        // Write your logic here
+        return "Row, row, row your boat\nGently down the stream!!!";
+
+    }
+}
---
+++ src/MyFirstClass.java
@@ -0,0 +1,8 @@
+public class MyFirstClass {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = x+2;
+        int z = x+y;
+        System.out.println("Z Value is :: "+z);
+    }
+}
